ac295c91c62b171ab29ed516bdd817a6c35f4526
Laxmipriya-1 <150228943+Laxmipriya-1@users.noreply.github.com>
2026-03-11 21:51:03 +0530

    setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [movie, setMovie] = useState([]);
+  const [favorites, setFavorites] = useState([]);
+
+  return (
+    < >
+
+    </>
+  )
+}
+
+export default App
