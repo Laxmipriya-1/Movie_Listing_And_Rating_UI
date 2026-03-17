8efed50172da020e93231e949f98dfc52c60c6dd
Laxmipriya-1 <150228943+Laxmipriya-1@users.noreply.github.com>
2026-03-17 13:15:03 +0530

    Filteration, Responsive and Code Completion

---
+++ src/components/MovieList/FilterGroup.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const FilterGroup = ({minRating , onRatingClick , ratings}) => {
+  return (
+    <div>
+         <ul className='align_center movie_filter'>
+         {
+            ratings.map(rate => (
+            <li className= {minRating === rate ? 'movie_filter_item active' : 'movie_filter_item'}
+            key={rate}
+            onClick={() => 
+            onRatingClick(rate)}>{rate}+ Star</li>
+             ))}
+          </ul>
+    </div>
+  )
+}
+
+export default FilterGroup
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,64 @@
-import React from 'react'
+// import React from 'react'
+// import "./Navbar.css";
+// import Fire from "../../assets/fire.png"
+// import Star from "../../assets/star.png"
+// import Party from "../../assets/partying-face.png"
+
+// const Navbar = () => {
+//   return (
+//     <nav className='navbar'>
+//       <h1>MOVIE L&R</h1>
+       
+//       <div className="navbar_links">
+//         <a href='#popular'>Popular<img src={Fire} alt='fire emoji' className='navbar_emoji'/></a>
+//         <a href='#top_rated'>Top Rated<img src={Star} alt='fire emoji' className='navbar_emoji'/></a>
+//         <a href='#upcoming'>Upcoming<img src={Party} alt='fire emoji' className='navbar_emoji'/></a>
+//       </div>
+//     </nav>
+//   )
+// }
+
+// export default Navbar
+
+import React, { useState } from 'react'
 import "./Navbar.css";
 import Fire from "../../assets/fire.png"
 import Star from "../../assets/star.png"
 import Party from "../../assets/partying-face.png"
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className='navbar'>
       <h1>MOVIE L&R</h1>
-       
-      <div className="navbar_links">
-        <a href="">Popular<img src={Fire} alt='fire emoji' className='navbar_emoji'/></a>
-        <a href="">Top Rated<img src={Star} alt='fire emoji' className='navbar_emoji'/></a>
-        <a href="">Upcoming<img src={Party} alt='fire emoji' className='navbar_emoji'/></a>
+
+      {/* Hamburger */}
+      <div 
+        className={`hamburger ${isOpen ? "active" : ""}`} 
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
       </div>
 
+      {/* Links */}
+      <div className={`navbar_links ${isOpen ? "show" : ""}`}>
+        <a href='#popular' onClick={() => setIsOpen(false)}>
+          Popular <img src={Fire} alt='fire emoji' className='navbar_emoji'/>
+        </a>
 
+        <a href='#top_rated' onClick={() => setIsOpen(false)}>
+          Top Rated <img src={Star} alt='star emoji' className='navbar_emoji'/>
+        </a>
+
+        <a href='#upcoming' onClick={() => setIsOpen(false)}>
+          Upcoming <img src={Party} alt='party emoji' className='navbar_emoji'/>
+        </a>
+      </div>
     </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
